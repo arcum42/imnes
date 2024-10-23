14b5ca2c2b8472cc80a4d2979a42e09447315da9
Shanoah Alkire <arcum42@gmail.com>
2024-10-23 02:38:18 -0700

    Add a main menu bar and controls window, and allow hiding and showing windows.

--- src/main.cpp
+++ src/main.cpp
@@ -15,53 +15,87 @@ import imgui_sdl3_wrap;
 import log;
 
 bool quit = false;
-bool demo = true;
 
-SDL_Window *main_window;
-SDL_Renderer *main_renderer;
+SDL_Window *main_window = nullptr;
+SDL_Renderer *main_renderer = nullptr;
 
 void draw_content()
 {
+    static bool show_demo = true;
+    static bool show_stats = true;
+    static bool show_log = true;
+
+    if (ImGui::BeginMainMenuBar())
+    {
+        if (ImGui::BeginMenu("Windows"))
+        {
+            if (ImGui::MenuItem("Stats", "", &show_stats))
+            {
+                if (!show_stats) 
+                    log::writeln("Hiding Stats Window");
+                else
+                    log::writeln("Showing Stats Window");
+            }
+            if (ImGui::MenuItem("Log", "", &show_log))
+            {
+                if (!show_log) 
+                    log::writeln("Hiding Log Window");
+                else
+                    log::writeln("Showing Log Window");
+            }
+            if (ImGui::MenuItem("Demo", "", &show_demo))
+            {
+                if (!show_demo) 
+                    log::writeln("Hiding Demo Window");
+                else
+                    log::writeln("Showing Demo Window");
+            }
+            ImGui::EndMenu();
+        }
+        ImGui::EndMainMenuBar();
+    }
+
+    ImGui::Begin("Controls");
+    if (ImGui::Button("Step"))
+        ;
+    if (ImGui::Button("Play"))
+        ;
+    if (ImGui::Button("Pause"))
+        ;
+    ImGui::End();
+
+    if (show_stats)
+    {
+        ImGui::Begin("Stats");
+        if (ImGui::CollapsingHeader("Registers"))
+        {
+            ImGui_Wrap::BulletText(std::format("PC: {}", cpu::reg.PC));
+            ImGui_Wrap::BulletText(std::format("AC: {}", cpu::reg.AC));
+            ImGui_Wrap::BulletText(std::format("X: {}", cpu::reg.X));
+            ImGui_Wrap::BulletText(std::format("Y: {}", cpu::reg.Y));
+            ImGui_Wrap::BulletText(std::format("SR: {}", cpu::reg.SR));
+            ImGui_Wrap::BulletText(std::format("SP: {}", cpu::reg.SP));
+        }
+        if (ImGui::CollapsingHeader("Flags"))
+        {
+            ImGui_Wrap::BulletText(std::format("Negative: {}", cpu::flag.N));
+            ImGui_Wrap::BulletText(std::format("Overflow: {}", cpu::flag.V));
+            ImGui_Wrap::BulletText(std::format("(Unused): {}", cpu::flag.Unused));
+            ImGui_Wrap::BulletText(std::format("Break: {}", cpu::flag.B));
+            ImGui_Wrap::BulletText(std::format("Decimal: {}", cpu::flag.D));
+            ImGui_Wrap::BulletText(std::format("Interrupt: {}", cpu::flag.I));
+            ImGui_Wrap::BulletText(std::format("Zero: {}", cpu::flag.Z));
+            ImGui_Wrap::BulletText(std::format("Carry: {}", cpu::flag.C));
+        }
+        ImGui::End();
+    }
+
     // Just for testing.
-    bool show_demo_window = true;
-    if (show_demo_window)
-        ImGui::ShowDemoWindow(&show_demo_window);
+    if (show_demo)
+        ImGui::ShowDemoWindow(&show_demo);
 
-    // Create context for ImGui widgets
-    static float globalCount = 0.0f;
-    ImGui::Begin("Hello World");
-    ImGui::Text("This is some useful text.");
-    ImGui::SliderFloat("My Slider", &globalCount, 0.0f, 10.0f);
-    if (ImGui::Button("Quit"))
-    {
-        quit = true;
-    }
-    ImGui::End();
-
-    ImGui::Begin("Stats");
-    if (ImGui::CollapsingHeader("Registers"))
-    {
-        ImGui_Wrap::BulletText(std::format("PC: {}", cpu::reg.PC));
-        ImGui_Wrap::BulletText(std::format("AC: {}", cpu::reg.AC));
-        ImGui_Wrap::BulletText(std::format("X: {}", cpu::reg.X));
-        ImGui_Wrap::BulletText(std::format("Y: {}", cpu::reg.Y));
-        ImGui_Wrap::BulletText(std::format("SR: {}", cpu::reg.SR));
-        ImGui_Wrap::BulletText(std::format("SP: {}", cpu::reg.SP));
-    }
-    if (ImGui::CollapsingHeader("Flags"))
-    {
-        ImGui_Wrap::BulletText(std::format("Negative: {}", cpu::flag.N));
-        ImGui_Wrap::BulletText(std::format("Overflow: {}", cpu::flag.V));
-        ImGui_Wrap::BulletText(std::format("(Unused): {}", cpu::flag.Unused));
-        ImGui_Wrap::BulletText(std::format("Break: {}", cpu::flag.B));
-        ImGui_Wrap::BulletText(std::format("Decimal: {}", cpu::flag.D));
-        ImGui_Wrap::BulletText(std::format("Interrupt: {}", cpu::flag.I));
-        ImGui_Wrap::BulletText(std::format("Zero: {}", cpu::flag.Z));
-        ImGui_Wrap::BulletText(std::format("Carry: {}", cpu::flag.C));
-    }
-    ImGui::End();
-
-    log::draw();
+    if (show_log)
+        log::draw();
 }
 
 bool init_sdl()
